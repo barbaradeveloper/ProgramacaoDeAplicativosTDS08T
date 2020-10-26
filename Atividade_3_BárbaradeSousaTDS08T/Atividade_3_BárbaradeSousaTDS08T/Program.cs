52821f386cd071e1282ce09ce81624f9cd3bdc8b
barbaradeveloper <barbaramartins1245@gmail.com>
2020-10-26 13:50:06 -0300

    Atividade 3 - Tipos de dados

---
+++ Atividade_3_BárbaradeSousaTDS08T/Atividade_3_BárbaradeSousaTDS08T/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Atividade_3_BD_BárbaradeSousaTDS08T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Questão 1 
+            sbyte y = 127;
+            Console.WriteLine(y);
+
+
+            int x = 235;
+            Console.WriteLine(x);
+
+            long n = 3453533873L;
+            Console.WriteLine(n);
+
+            byte n2 = 199;
+            n2++;
+            Console.WriteLine(n2);
+
+            float n3 = 9.90f;
+            Console.WriteLine(n3);
+
+            double n4 = 1000.49;
+            Console.WriteLine(n4);
+
+            decimal n5 = 200.30m;
+            Console.WriteLine(n5);
+
+            char genero = 'F';
+            Console.WriteLine(genero);
+
+            bool ligado = false;
+            Console.WriteLine(ligado);
+
+
+            // Questão 2 
+            int numeroCasa = 310;
+            Console.WriteLine(numeroCasa);
+
+
+            // Questão 3 
+            decimal habTerra = 8000000000;
+            Console.WriteLine(habTerra);
+
+            // Questão 4 
+            float salario = 5000f;
+            Console.WriteLine(salario);
+
+            // Questão 5 
+            int numCelular = 994593207;
+            Console.WriteLine(numCelular);
+
+            // Questão 6 
+            string nome = "Bárbara de Sousa Martins";
+            Console.WriteLine(nome);
+
+            // Questão 7 
+            /* Estou aprendendo c# na matéria de Programação de Aplicativos. */
+
+            // Questão 8 
+            char letra = '\u0156';
+            Console.WriteLine(letra);
+
+            // Questão 9 
+            ulong questao9 = 18446744073709551615;
+            Console.WriteLine(questao9);
+
+            // Questão 10 
+            long questao10 = -9223372036854775808L;
+            Console.WriteLine(questao10);
+        }
+    }
+}
