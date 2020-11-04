715ddbf8d2fa3ddafe9fbb47fcdefb95dd7fab7e
barbaradeveloper <barbaramartins1245@gmail.com>
2020-11-04 13:38:29 -0300

    Atividade 13 - Fixação: Construtor e sobrecarga

---
+++ Atividade_13_BárbaradeSousa_TDS08T/Atividade_13_BárbaradeSousa_TDS08T/Candidato.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Atividade_13_BárbaradeSousa_TDS08T
+{
+    class Candidato
+    {
+        public string Nome;
+        public string Partido;
+        public string Ficha;
+        public double Patrimonio;
+        public string Profissao;
+        public string Cargo;
+        public string Municipio;
+
+        public Candidato(string nome, string partido, double patrimonio, string cargo, string profissao)
+        {
+            Nome = nome;
+            Partido = partido;
+            Patrimonio = patrimonio;
+            Profissao = profissao;
+            Cargo = cargo;
+            Municipio = "Lagoa Santa";
+            Ficha = "Limpa";
+        }
+
+        public override string ToString()
+        {
+            return "Candidato " +  Nome + " cadastrado com sucesso! \r\n" 
+                + " Dados para conferência: \r\n" + 
+                " Candidato a " + Cargo + " de " + Municipio +  "\r\n"
+                + " Nome: " + Nome 
+                + "\r\n Partido:  " + Partido  
+                + "\r\n Patrimônio declarado: R$" + Patrimonio.ToString("F2", CultureInfo.InvariantCulture)
+                + "\r\n Profissão: "+ Profissao
+                + "\r\n Ficha limpa: "+ Ficha ;
+        }
+    }
+}
---
+++ Atividade_13_BárbaradeSousa_TDS08T/Atividade_13_BárbaradeSousa_TDS08T/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atividade_13_BárbaradeSousa_TDS08T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("             Programa Eleições Municipais             ");
+            Console.Write("Informe o nome do candidato: ");
+            string nome = Console.ReadLine();
+            Console.Write("Informe o Partido do candidato: ");
+            string partido = Console.ReadLine();
+            Console.Write("Informe o Patrimômio do candidato: ");
+            double patrimonio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Informe a profissão atual do candidato: ");
+            string profissao = Console.ReadLine();
+            Console.Write("Informe o Cargo pretendido pelo candidato: ");
+            string cargo = Console.ReadLine();
+
+
+            Candidato c = new Candidato(nome, partido, patrimonio, profissao, cargo);
+            Console.WriteLine(c);
+
+        }
+    }
+}
