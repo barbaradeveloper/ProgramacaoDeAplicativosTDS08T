15febebfb78daff8cc068a2210934a436d98f888
barbaradeveloper <barbaramartins1245@gmail.com>
2020-11-25 15:03:13 -0300

    Atividade 19 - Fixação Vetor

---
+++ Atividade_19_BárbaradeSousa_TDS08T/Atividade_19_BárbaradeSousa_TDS08T/Estudante.cs
@@ -0,0 +1,26 @@
+﻿namespace Atividade_19_BárbaradeSousa_TDS08T
+{
+    class Estudante
+    {
+        // prop tab + tab
+        // Encapsulamento - serve para segurança
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        // Construtor
+        public Estudante(string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public override string ToString()
+        {
+            return Quarto + ": "
+                        + Nome + ", "
+                        + Email;
+        }
+    }
+}
---
+++ Atividade_19_BárbaradeSousa_TDS08T/Atividade_19_BárbaradeSousa_TDS08T/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Atividade_19_BárbaradeSousa_TDS08T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                        PROGRAMA DE ALGUÉL DE UM PENSIONATO ");
+            Console.WriteLine();
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            // O tipo do vetor vai ser o nome da classe
+            Estudante[] cliente = new Estudante[10]; // vetor de 10 posições
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Aluguel #" + (i+1) + ":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                cliente[quarto] = new Estudante(nome, email, quarto);                            
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (cliente[i] != null)
+                {
+                    Console.WriteLine(cliente[i]);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
