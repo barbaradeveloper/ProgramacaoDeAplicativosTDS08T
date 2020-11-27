7bf550c6dd0c8fb4692018d4cca1772497ea11f3
barbaradeveloper <barbaramartins1245@gmail.com>
2020-11-27 11:36:43 -0300

    Atividade de fixação - Vetor e ForEach

---
+++ Atividade_de_Fixação_BárbaradeSousa_TDS08T/Atividade_de_Fixação_BárbaradeSousa_TDS08T/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Atividade_de_Fixação_BárbaradeSousa_TDS08T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Questão 1 
+            Console.WriteLine("               PROGRAMA QUE LÊ 6 VALORES INTEIROS E MOSTRA OS VALORES LIDOS ");
+            Console.WriteLine();
+
+
+            int[] numeros = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("Informe o valor do " + i + "° número inteiro: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int i in numeros)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("---------------------===========================------------------------");
+
+            //  Questão 2 
+            Console.WriteLine("             PROGRAMA QUE LÊ 6 NOMES E MOSTRA NA TELA OS VALORES LIDOS ");
+            Console.WriteLine();
+
+            string[] nomes = new string[6];
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("Informe o " + i + "° nome: ");
+                nomes[i] = Console.ReadLine();
+            }
+            foreach (string i in nomes)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
